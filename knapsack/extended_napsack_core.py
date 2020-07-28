2c0903d624e16c6289194e18fad24cd70034ffaa
A.L <32211153+iluvjava@users.noreply.github.com>
2020-07-27 20:36:45 -0700

    l

--- knapsack/extended_napsack_core.py
+++ knapsack/extended_napsack_core.py
@@ -24,8 +24,13 @@ class EknapsackProblem:
         indicating the items and the sub-problem can USE for ITS SOLUTION.
 
         **
-            if I in indices: Then, I can be added to the knapsack.
+            If I in indices: Then, I can be added to the knapsack.
             If I not in indices:
+                If I is in the partial solution, then it's constrained by <= floor(x_i) y the parent nodes in the BB
+                tree.
+                If I is not in the partial solution, then something is wrong, because the variable is free, but
+                    it's not investigated any this problem and any of the sub-problems of this sub-problem.
+
     """
     def __init__(self,
                  profits: RealNumberList,
@@ -42,7 +47,6 @@ class EknapsackProblem:
         assert all(I >= 0 for I in counts), "Item's counts cannot be negative."
         assert all(len(I) == self.Size for I in [profits, weights, counts, budget])
         self._P, self._W, self._C, self._B = profits, weights, counts, budget
-
 
     def greedy_solve(self, AlreadyDecidedSoln):
         """
@@ -70,24 +74,43 @@ class EknapsackProblem:
             Objective value of the solution.
         """
         def SubSlicing(self, toSlice):
+            # Copied.
             return [toSlice for I in self.Indices]
 
         def SubSolving(P, W, C, B):
             Values = [((P[I]/W[I], I) if W[I] != 0 else float("+inf")) for I in len(P)]
             Values.sort(key=(lambda x: x[0]), reverse=True)
             Values = [I for _, I in Values]
-            FractionalItems, IntegralItems = -1, {}
+            FractionalItems, IntegralItems, ObjectiveValue = {}, {}, 0
             RemainingBudge = B
             for Idx in Values:
-
-                pass
-
-        P, W, C, B = SubSlicing(self._P),  SubSlicing(self.W),  SubSlicing(self.C),  SubSlicing(self._B)
+                if W[Idx] == 0:
+                    ObjectiveValue += P[Idx]
+                    IntegralItems[Idx] = C[Idx]
+                else:
+                    if RemainingBudge == 0:
+                        break
+                    ToTake = min(RemainingBudge/W[Idx], C[Idx])
+                    if int(ToTake) == ToTake:
+                        IntegralItems[Idx] = ToTake
+                    else:
+                        FractionalItems[Idx] = ToTake
+                    RemainingBudge -= ToTake*W[Idx]
+                    ObjectiveValue += ToTake*P[Idx]
+            return IntegralItems, FractionalItems, ObjectiveValue
+        # Problem Digest------------------------------------------------------------------------------------------------
+        C = self._C.copy()
+        for K, V in AlreadyDecidedSoln:
+            # Take partial solution into account.
+            C[K] -= V
+        P, W, C, B = SubSlicing(self._P),  SubSlicing(self.W),  SubSlicing(C),  SubSlicing(self._B)
         # The index in the sub problem is remapped to indices in the original problem.
         IdxInverseMap = [-1]*self.Size
         for I, V in self.Indices:
             IdxInverseMap[I] = V
-
+        # End ----------------------------------------------------------------------------------------------------------
+        # Merge the solution -------------------------------------------------------------------------------------------
+        IntegralItems, FractionalItems, ObjVal = SubSolving(P, W, C, B)
 
 
         pass
