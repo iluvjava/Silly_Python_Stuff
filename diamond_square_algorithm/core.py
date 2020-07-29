022cc2a88c0a62a62cb5c48ca730c320c43c033f
Alto Legato <victor1301166040@gmail.com>
2020-07-28 23:36:49 -0700

    d

---
+++ diamond_square_algorithm/core.py
@@ -0,0 +1,6 @@
+"""
+    In this file, we are going to implement, visualize and investigate the diamond square algorithm
+    for realistic terran generations.
+
+
+"""
--- knapsack/extended_napsack_core.py
+++ knapsack/extended_napsack_core.py
@@ -11,6 +11,8 @@
         from further recursion.
     ! Yes, for standard knapsack, if we lower the counts of item that has been selected for the greedy solution,
     then that item will still be selected for the number of counts in future recursion.
+
+    TODO: Check the correctness of the algorithm with the Pul LP solver.
 """
 from typing import *
 
@@ -18,12 +20,12 @@ RealNumberList = List[Union[float, int]]
 
 class EknapsackProblem:
     """
-        * problem Description:
+        * Preconditions:
             ** The item's weight are positive real numbers.
             ** The item's profits are positive real numbers.
             ** The item's counts are positive real numbers.
             ** The total budge allowed.
-        This represents an extendned knapsack problem.
+        This represents an extended knapsack problem.
         * It will use as little resources as possible so it's efficient for the branch and bound algorithm.
 
         * The whole problem is encapsulated in an instance of the class,
@@ -64,8 +66,12 @@ class EknapsackProblem:
         self._P, self._W, self._C, self._B = profits, weights, counts, budget
 
         # Initialize as an root problem for bb
-        self._PartialSoln = dict(zip(range(self.Size), [0]*self.Size))
+        self._PartialSoln = {}
         self._Indices = set(range(self.Size))
+
+        # The instance is only going to solve itself ONCE.
+        self.__GreedySoln, self._ObjVal, self._FractIndx = None, None, None
+
 
     def greedy_solve(self):
         """
@@ -77,6 +83,9 @@ class EknapsackProblem:
             further solution.
 
             ** Function will modify the partial solution that for this class, a reference will be returned.
+
+            ** If the objective value return is negative infinity, then bingo, the passed in partial solution
+            is not feasible.
         :param AlreadyDecidedSoln:
             The constraints accumulated via the BB algorithm, limiting the options for choosing
             further solution.
@@ -94,6 +103,9 @@ class EknapsackProblem:
             ,
             Objective value of the solution.
         """
+        if self._ObjVal is not None:
+            return self.__GreedySoln, self._ObjVal, self._FractIndx
+
         def SubSlicing(toSlice, Indices):
             # Copied.
             return [toSlice[I] for I in Indices]
@@ -117,21 +129,22 @@ class EknapsackProblem:
 
         # Problem Digest------------------------------------------------------------------------------------------------
         C = self._C.copy()
-        AlreadyDecidedSoln = self._PartialSoln
+        AlreadyDecidedSoln = self._PartialSoln.copy()
         for K, V in AlreadyDecidedSoln.items():
             # Take partial solution into account.
             C[K] -= V
         B = self._B - sum(self._W[K]*V for K, V in AlreadyDecidedSoln.items())
+        # Infeasible, Pruned -------------------------------------------------------------------------------------------
+        if B < 0:
+            self._ObjVal = float("-inf")
+            return None, float("-inf"), None
         P, W, C = SubSlicing(self._P, self.Indices), SubSlicing(self._W, self.Indices), SubSlicing(C, self.Indices)
-
-        # Index in sub |==> Index in full problem.
-        IdxInverseMap = [-1]*self.Size
+        IdxInverseMap = [-1]*self.Size   # Index in sub |==> Index in full problem.
         for I, V in enumerate(self.Indices):
             IdxInverseMap[I] = V
-        # End ----------------------------------------------------------------------------------------------------------
         # solve and merge the solution ---------------------------------------------------------------------------------
         Soln = SubSolving(P, W, C, B)
-        SolnRemapped, FracIdx= {}, -1
+        SolnRemapped, FracIdx = {}, -1
         for K, V in Soln.items():
             SolnRemapped[IdxInverseMap[K]] = V
         for K, V in SolnRemapped.items():
@@ -141,22 +154,27 @@ class EknapsackProblem:
                 AlreadyDecidedSoln[K] = V
             else:
                 AlreadyDecidedSoln[K] += V
-        # End ----------------------------------------------------------------------------------------------------------
-        return AlreadyDecidedSoln, sum(self._P[K]*V for K, V in AlreadyDecidedSoln.items()), FracIdx
 
-    def branch(self, globalIntegralValue):
+        self.__GreedySoln = AlreadyDecidedSoln
+        self._ObjVal = sum(self._P[K]*V for K, V in AlreadyDecidedSoln.items())
+        self._FractIndx = FracIdx
+        return self.__GreedySoln, self._ObjVal, self._FractIndx
+
+    def branch(self, globalIntegralValue, NewSoln=None):
         """
             Branch this current instance.
         :param BestIntegralValue:
-            Provide a best integral value, it's a must for the Heuristic Brancher.
+            Provide a best integral value, it's a must for the Heuristic brancher.
+        :param NewSoln:
+            The best global integral solution, if not current integral solutions are found or it's not as good as
+            the given one, it will just return this parameter.
         :return:
             Sub-problems, and renewed objective value and solution for the global landscape.
         """
         Soln, ObjVal, FracIdx = self.greedy_solve()
         # To Return:----------------------------------------------------------------------------------------------------
-        NewSoln, NewObjVal, SubP1, SubP2, = None, None, None, None
-        # END ----------------------------------------------------------------------------------------------------------
-        IsIntegral = FracIdx != -1
+        NewObjVal, SubP1, SubP2, = globalIntegralValue, None, None
+        IsIntegral = FracIdx == -1
         OptimalitySatisfied = ObjVal > globalIntegralValue
         # Pruned by optimality of the integral solution ----------------------------------------------------------------
         if IsIntegral and OptimalitySatisfied:
@@ -165,27 +183,41 @@ class EknapsackProblem:
         # Fractional, and it should branch -----------------------------------------------------------------------------
         if OptimalitySatisfied:
             # p1, Bound from above -------------------------------------------------------------------------------------
-            PartialSoln = Soln.Copy()
+            PartialSoln = self.PartialSoln.copy()
             NewIndices = self.Indices
             NewIndices.remove(FracIdx)
-            if int(PartialSoln[FracIdx]) != 0:
-                PartialSoln[FracIdx] = int(PartialSoln[FracIdx])
-            else:
-                del PartialSoln[FracIdx]
+            if int(Soln[FracIdx]) != 0:
+                PartialSoln[FracIdx] = int(Soln[FracIdx])
             SubP1 = EknapsackProblem(self._P, self._W, self._C, self._B)
             SubP1.Indices = NewIndices
             SubP1.PartialSoln = PartialSoln
             # p2, Bound from below -------------------------------------------------------------------------------------
-            PartialSoln = Soln.Copy()
+            PartialSoln = self.PartialSoln.copy()
             NewIndices = self.Indices
-            PartialSoln[FracIdx] = int(PartialSoln[FracIdx]) + 1
+            PartialSoln[FracIdx] = int(Soln[FracIdx]) + 1
             SubP2 = EknapsackProblem(self._P, self._W, self._C, self._B)
             SubP2.Indices = NewIndices
             SubP2.PartialSoln = PartialSoln
 
         return NewSoln, NewObjVal, SubP1, SubP2
 
+    def __repr__(self):
+        s = "\n" + "-"*20 + "\n"
+        s += "EKnapSack Instance: \n"
+        s += f"Size: {self.Size}\n"
+        s += f"Non-Fixed Variables: {self.Indices}\n"
+        s += f"Partial Solution: {self.PartialSoln}\n"
+        s += f"Greedy Soluion: {self.__GreedySoln}\n"
+        s += f"Upperbound (Objective Value from Greedy algo): {self._ObjVal}\n"
+        return s
 
+    def __getitem__(self, I):
+        """
+            Quick tuple indexing to get desirable information regarding a certain item.
+        :param item:
+        :return:
+        """
+        return [self._P, self._W, self._C][I[0]][I[1]]
     @property
     def Indices(self):
         return self._Indices.copy()
@@ -213,16 +245,35 @@ class EknapsackProblem:
         self._PartialSoln = item
 
     @staticmethod
-    def BB():
+    def BB(rootProblem):
         """
             A static method for evaluation the whole Eknapsack problem
-
+        :param rootProblem:
+            An instance of an root problem, representing the initial problem for the initial knapsack.
         :return:
-            solution.
+            optimal solution.
         """
+        def Initialization():
+            S, _, _ = rootProblem.greedy_solve()
+            S = dict([(I, V) for I, V in S.items() if int(V) == V])
+            ObjVal = sum(rootProblem[0, I]*V for I, V in S.items())
+            print(f"BB executing with war start solution and objective value: ")
+            print(S)
+            print(ObjVal)
+            return S, ObjVal
+        GIntSoln, GObjVal = Initialization()
+        Stack = [rootProblem]
+        while len(Stack) != 0:
+            P = Stack.pop()
+            P.greedy_solve()
+            print(P)
+            GIntSoln, GObjVal, SubP1, SubP2 = P.branch(GObjVal, GIntSoln)
+            if SubP1 is not None:
+                Stack.append(SubP1)
+            if SubP2 is not None:
+                Stack.append(SubP2)
+        return GIntSoln, GObjVal
 
-
-        pass
 
 
 def main():
@@ -230,20 +281,46 @@ def main():
     def TestKnapSack():
         P, W, B, C = [2, 3, 2, 1], [6, 7, 4, 1], 9, [1]*4
         EKnapSack = EknapsackProblem(P, W, C, B)
-        print(f"Instance: {EKnapSack}")
-        print(EKnapSack.greedy_solve())
 
-        print("exclude item at index 1 (set x_1 = 0)")
+        print(EKnapSack.greedy_solve())
         EKnapSack.Indices = {0, 2, 3}
         del EKnapSack.PartialSoln[1]
+
+        print(EKnapSack)
         print(EKnapSack.greedy_solve())
-        print("Including item at index 1 (set x_1 = 1)")
+
         EKnapSack.Indices = {0, 1, 2, 3}
         EKnapSack.PartialSoln = {1: 1}
+        print(EKnapSack)
         print(EKnapSack.greedy_solve())
 
         pass
-    TestKnapSack()
+
+    def TestKnapSakBranchingMechanics():
+        P, W, B, C = [2, 3, 2, 1], [6, 7, 4, 1], 9, [1] * 4
+        EKnapSack = EknapsackProblem(P, W, C, B)
+        print(EKnapSack)
+        _, _, Sub1, Sub2 = EKnapSack.branch(float("-inf"))
+        print(f"Sub1 {Sub1}")
+        print(f"Sub2 {Sub2}")
+
+    def RunBB():
+        P, W, B, C = [2, 3, 2, 1], [6, 7, 4, 1], 9, [1] * 4
+        EKnapSack = EknapsackProblem(P, W, C, B)
+        print(EknapsackProblem.BB(EKnapSack))
+        print("="*20)
+        P, W, B, C = [2, 3, 2, 1], [6, 7, 4, 1], 9, [1, 1, 1, 2]
+        EKnapSack = EknapsackProblem(P, W, C, B)
+        print(EknapsackProblem.BB(EKnapSack))
+        print("="*20)
+        P, W, B, C = [1, 2, 3, 4], [4, 3, 2, 1], 8, [4, 2, 1, 1]
+        EKnapSack = EknapsackProblem(P, W, C, B)
+        print(EknapsackProblem.BB(EKnapSack))
+        P, W, B, C = [1, 2, 3, 4], [4, 3, 2, 1], 8, [4, 2, 1, 4]
+        EKnapSack = EknapsackProblem(P, W, C, B)
+        print(EknapsackProblem.BB(EKnapSack))
+
+    RunBB()
 
 
 if __name__ == "__main__":
