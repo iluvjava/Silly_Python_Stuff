edd3362bb7a1463a95b252768413a520894f2fca
Alto Legato <victor1301166040@gmail.com>
2020-09-08 00:29:48 -0700

    d

---
+++ ml_related/tensor_flow/main.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+#@title Import relevant modules
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# The following lines adjust the granularity of reporting.
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+print("Imported modules.")
+
